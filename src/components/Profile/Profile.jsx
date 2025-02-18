f1b842b8be4c67602a99ee1c9090f9c4489079b4
your_name <orkunkorcak@gmail.com>
2025-02-18 22:34:40 +0300

    profile css added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,30 @@
-const Profile = ({ name, tag, location, image, stats }) => {
-    return (
-    <div>
-        <div>
-            <img
-                src={image}
-                alt="User avatar"
-            />
-            <p>{name}</p>
-            <p>@{tag}</p>
-            <p>{location}</p>
-        </div>
+import css from "./Profile.module.css"
 
-        <ul>
-            <li>
-                <span>Followers</span>
-                <span>{stats.followers}</span>
-            </li>
-            <li>
-                <span>Views</span>
-                <span>{stats.views }</span>
-            </li>
-            <li>
-                <span>Likes</span>
-                <span>{stats.likes}</span>
-            </li>
-        </ul>
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={css.container}>
+      <div className={css.imgdiv}>
+        <img className={css.img} src={image} alt="User avatar" />
+        <p className={css.name}>{name}</p>
+        <p className={css.ptaglctn}>@{tag}</p>
+        <p className={css.ptaglctn}>{location}</p>
+      </div>
+
+      <ul className={css.list}>
+        <li className={css.listItem}>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li className={css.listItem}>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li className={css.listItem}>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
     </div>
-)}
-export default Profile
+  );
+};
+export default Profile;
